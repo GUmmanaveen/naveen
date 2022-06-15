900a945da0d23bf90797355dd4cee79acda6942f
Naveen <you@example.com>
2022-06-15 10:33:00 +0530

    my first commit

---
+++ Assignment2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Enter your firstName"
+read firstName
+if [[ "${firstName}" =~ [^a-zA-Z] ]]; then
+    echo Enter a valid firstName using alphabets
+else
+echo "Enter your lastName"
+   read lastName
+if [[ "${lastName}" =~ [^a-zA-Z] ]];
+then 
+echo Enter a valid lastName using alphabets
+else
+echo "$firstName.$lastName"
+echo "$firstName" "$lastName"|sha1sum
+fi
+fi
